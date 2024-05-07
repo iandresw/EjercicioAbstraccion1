cd4b2a4fb76b4310322ea5a543eafeec5d62a409
iandresw <rbejarano@unah.hn>
2024-05-06 22:06:53 -0600

    Main Principal
    Se implemento la clase calculadora para poner a prueba los métodos abstractos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EjercicioAbstraccion;
+
 Console.WriteLine("Hello, World!");
+Calculadora calculadora = new Calculadora();
+Console.WriteLine("Ingrese el Primer Numero:");
+calculadora.num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el Segundo Numero:");
+calculadora.num2 = double.Parse(Console.ReadLine());
+calculadora.Imprimir();
+
+
