2f4146c03728c3c50da3c0d166f744e00f9db90a
iandresw <rbejarano@unah.hn>
2024-05-06 22:01:38 -0600

    Clase abstracta Operaciones
    Se creo la clase operaciones con las 4 operaciones básicas (Suma, Resta, División, Multiplicación)

---
+++ Operaciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    public abstract class Operaciones()
+    {
+        public abstract double Suma();
+        public abstract double Resta();
+        public abstract double Multiplicacion();
+        public abstract double Divicion();
+        public abstract void Imprimir();
+     
+    }
+}
