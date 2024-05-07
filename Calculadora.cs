8e6779af025ff2bb6ea477a2f1f96a71716463ea
iandresw <rbejarano@unah.hn>
2024-05-06 22:05:45 -0600

    Clase Calculadora
    -Se creo la clase calculadora que hereda la clase abstracta Operaciones
    -Implementación los métodos abstractos heredados de Operaciones
     -Atributo num1 y num2 creados.
    -Funcionalidad a los métodos abastractos

---
+++ Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    public class Calculadora : Operaciones
+    {
+        public double num1{ get; set; }
+        public double num2 { get; set; }
+
+        public override double Divicion()
+        {
+            return num1 / num2; 
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Suma: {Suma()}");
+            Console.WriteLine($"La Resta: {Resta()}");
+            Console.WriteLine($"La Multiplicacion: {Multiplicacion()}");
+            Console.WriteLine($"La Divicion: {Divicion()}");
+        }
+
+        public override double Multiplicacion()
+        {
+            return num1 * num2;
+        }
+
+        public override double Resta()
+        {
+            return (num1 - num2);
+        }
+
+        public override double Suma()
+        {
+           return  num1 + num2;
+        }
+    }
+}
